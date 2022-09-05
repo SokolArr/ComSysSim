a8b028f351e800775e231072591ed6adbc9f49c5
Sokol Arr <axindri@gmail.com>
2022-09-05 21:52:31 +0300

    test gui

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
+# import sys
+
+# from PyQt6.QtCore import QSize, Qt
+# from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# # Subclass QMainWindow to customize your application's main window
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+
+#         self.setWindowTitle("My App")
+#         button = QPushButton("Press Me!")
+
+#         # Set the central widget of the Window.
+#         self.setCentralWidget(button)
+
+
+# app = QApplication(sys.argv)
+
+# window = MainWindow()
+# window.show()
+
+# app.exec()
+
+
+from test_gui import Ui_MainWindow
+
 import sys
-
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
 
-# Subclass QMainWindow to customize your application's main window
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
 
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
-
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
-
-
-app = QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
-
-app.exec()
+class MyMainWindow (QMainWindow, Ui_MainWindow):
+    def __init__(self, parent = None):
+        super(MyMainWindow, self).__init__(parent)
+        self.setupUi(self)
+        
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myWin = MyMainWindow()
+    myWin.show()
+    sys.exit(app.exec())
