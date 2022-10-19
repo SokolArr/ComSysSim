d4922a16013ec022d28a8916e4dd2511f22f3c3f
Sokol Arr <axindri@gmail.com>
2022-10-20 01:00:49 +0300

    QAM mod half done

--- engine_PSK.py
+++ engine_PSK.py
@@ -6,23 +6,25 @@ from ModulationPy import PSKModem
 
 import matplotlib.pyplot as plt
 
-
-modem = PSKModem(4, np.pi/4, 
-                 bin_input=False,
-                 soft_decision=False,
-                 bin_output=False)
+noise_coef = 5
+modem = PSKModem(32, np.pi/4, 
+                 bin_input=True,
+                 soft_decision=True,
+                 bin_output=True)
 
 # msg = np.array([0, 0, 0, 1, 1, 0, 1, 1]) # input message
 # msg = np.array([1,2,3,2,1,2,1,1,0,2,3,1,0,0,0,2,3,0,0]) # input message
 # msg = np.random.sample(10)
 
-rand_list=[]
-n=50
-for i in range(n):
-    rand_list.append(random.randint(0,3))
+# rand_list=[]
+# n=100
 
-msg = np.array(rand_list)
+# for i in range(n):
+#     rand_list.append(random.randint(0,3))
 
+msg = np.array([1,0,1,0,1,0,1,0,1])
+
+print (msg)
 
 msg_x = np.repeat(range(len(msg)), 2)
 msg_y = np.repeat(msg, 2)
@@ -32,11 +34,13 @@ msg_x = np.append(msg_x,msg_x[-1] + 1)
 msg_y = np.append(msg_y, msg_y[-1])
 
 
-os.system('clear')
+# os.system('clear')
 
 modulated = modem.modulate(msg) # modulation
 
-print(str(modulated))
+print (str(modulated))
+
+# print(str(modulated))
 
 new_modulated = np.copy(modulated)
 
@@ -47,10 +51,10 @@ new_modulated = np.copy(modulated)
 for index in range(new_modulated.size):
     # new_modulated[index].real = modulated[index].real + random.randint(0, 10)/100
     # new_modulated[index].imag = modulated[index].imag + random.randint(0, 10)/100
-    new_modulated[index] = new_modulated[index] + (random.randint(0, 10)/100 + random.randint(0, 10)/100j)
+    new_modulated[index] = new_modulated[index] + (random.randint(0, noise_coef)/100 + random.randint(0, noise_coef)/100j)
     
 
-print(str(new_modulated))
+# print(str(new_modulated))
 
 
 
@@ -78,39 +82,33 @@ mod_imags = [ele.imag for ele in modulated]
 n_mod_imags = [ele.imag for ele in new_modulated]
 
 # fig, axs = plt.subplots(3)
-fig, axs = plt.subplots(1, 3, figsize=(10, 4.5), dpi = 90)
+fig, axs = plt.subplots(1, 2, figsize=(10, 4.5), dpi = 90)
 fig.tight_layout()
 
 # fig.suptitle('msg and modulated signal')
 fig.canvas.manager.set_window_title('Graph 1')
 
-axs[0].plot(msg_x, msg_y, msg, 'ro')
-axs[0].set_title('Сигнал (сообщение)')
+axs[0].plot(msg_x, msg_y,)
+axs[0].set_title('Сигнал (сообщение) и демодулированный сигнал')
 axs[0].set_xlabel('t')
 axs[0].set_ylabel('Амплитуда')
 
 
 axs[1].scatter(mod_reals, mod_imags)
-axs[1].set_title('Модулированный сигнал')
+axs[1].set_title('Комплексное представление сигнала')
 axs[1].set_xlabel('Реальная часть')
 axs[1].set_ylabel('Мнимая часть')
 axs[1].grid()
-axs[1].set_xlim(-1,1)
-axs[1].set_ylim(-1,1)
+# axs[1].set_xlim(-1,1)
+# axs[1].set_ylim(-1,1)
 
 axs[1].scatter(n_mod_reals, n_mod_imags)
-# axs[1].set_title('Модулированный сигнал c ошибками среды распр.')
-axs[1].set_xlabel('Реальная часть')
-axs[1].set_ylabel('Мнимая часть')
-axs[1].set_xlim(-1,1)
-axs[1].set_ylim(-1,1)
 
-axs[1].legend(['Модулированный сигнал','Модулированный сигнал c ошибками среды распр.'])
+axs[1].legend(['Модулированный сигнал, идеальная среда','Модулированный сигнал c ошибками среды распр.'])
 
-axs[2].plot(dmsg_x, dmsg_y, demodulated, 'ro')
-axs[2].set_title('Демодулированный сигнал')
-axs[2].set_xlabel('t')
-axs[2].set_ylabel('Амплитуда')
+axs[0].plot(dmsg_x, dmsg_y,)
+
+axs[0].legend(['Сигнал (сообщение)','Демодулированный сигнал'])
 
 plt.show()
 
--- main.py
+++ main.py
@@ -1,44 +1,134 @@
-# import sys
+from PyQt5.QtWidgets import*
+from PyQt5.uic import loadUi
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
 
-# from PyQt6.QtCore import QSize, Qt
-# from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import (
+    QMainWindow, QApplication,
+    QLabel, QToolBar, QAction, QStatusBar
+)
 
+import plot_PSK as PSK
+     
+class MatplotlibWidget(QMainWindow):
+    mes = []
+    def __init__(self):
+        
+        QMainWindow.__init__(self)
 
-# # Subclass QMainWindow to customize your application's main window
-# class MainWindow(QMainWindow):
-#     def __init__(self):
-#         super().__init__()
-
-#         self.setWindowTitle("My App")
-#         button = QPushButton("Press Me!")
-
-#         # Set the central widget of the Window.
-#         self.setCentralWidget(button)
-
-
-# app = QApplication(sys.argv)
-
-# window = MainWindow()
-# window.show()
-
-# app.exec()
-
-
-from test_gui import Ui_MainWindow
-
-import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
-
-
-class MyMainWindow (QMainWindow, Ui_MainWindow):
-    def __init__(self, parent = None):
-        super(MyMainWindow, self).__init__(parent)
-        self.setupUi(self)
+        loadUi("run2.ui",self)
         
         
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    myWin = MyMainWindow()
-    myWin.show()
-    sys.exit(app.exec())
+        # self.apply_QAM_but.stateChanged.connect(self.check_apply_but_QAM)
+        self.apply_PSK_but.stateChanged.connect(self.check_apply_but_PSK)
+        self.plot_QAM.clicked.connect(self.check_but_QAM)
+        
+        self.combobox_signal_type_QAM.currentTextChanged.connect(self.signal_handler)
+        self.combobox_coef_QAM.currentTextChanged.connect(self.coef_handler)
+        self.combobox_decision_QAM.setEnabled(False)
+        
+        
+        self.addToolBar(NavigationToolbar(self.plot_area_wid.canvas, self))
+
+    def check_but_QAM(self):
+        if self.apply_QAM_but.isChecked() == True:
+            self.apply_PSK_but.setChecked(False)
+            if self.combobox_signal_type_QAM.currentText() == 'Binary':
+               bin_input = True
+               bin_output = True
+            if self.combobox_signal_type_QAM.currentText() == 'Integers':
+               bin_input = False
+               bin_output = False
+               
+            if self.combobox_decision_QAM.currentText() == 'Not soft':
+                soft_decis = False
+            if self.combobox_decision_QAM.currentText() == 'Soft':
+                soft_decis = True
+            if self.combobox_grey_QAM.currentText() == 'Yes':
+                grey_cod = True
+            if self.combobox_grey_QAM.currentText() == 'No':
+                grey_cod = False
+            inpText = self.QAM_input.text()
+            mes = list(map(int, inpText))
+            M = self.combobox_coef_QAM.currentText()
+            noise_coef = self.combobox_noise_QAM.currentText()
+            phase = self.combobox_phase_QAM.currentText()
+            
+            msg_x = np.repeat(range(len(mes)), 2)
+            msg_y = np.repeat(mes, 2)
+            msg_x = msg_x[1:]
+            msg_y = msg_y[:-1]
+            msg_x = np.append(msg_x,msg_x[-1] + 1)
+            msg_y = np.append(msg_y, msg_y[-1])
+            
+            
+            mod, newMod, demod = PSK.runEngine(mes, int(M), int(noise_coef), int(phase), grey_cod, bin_input, soft_decis, bin_output)
+        
+            dmsg = demod
+            dmsg_x = np.repeat(range(len(dmsg)), 2)
+            dmsg_y = np.repeat(dmsg, 2)
+            dmsg_x = dmsg_x[1:]
+            dmsg_y = dmsg_y[:-1]
+            dmsg_x = np.append(dmsg_x,dmsg_x[-1] + 1)
+            dmsg_y = np.append(dmsg_y, dmsg_y[-1])
+            
+            mod_reals = [ele.real for ele in mod]
+            n_mod_reals = [ele.real for ele in newMod]
+            # extract imaginary part
+            mod_imags = [ele.imag for ele in mod]
+            n_mod_imags = [ele.imag for ele in newMod]
+            
+            
+            self.plot_area_wid.canvas.axes.clear()
+            self.plot_area_wid.canvas.axes.plot(msg_x, msg_y, dmsg_x, dmsg_y)
+            self.plot_area_wid.canvas.axes.grid()
+            self.plot_area_wid.canvas.draw()
+    
+    def signal_handler(self):
+        if int(self.combobox_coef_QAM.currentText()) < 32:
+            pow = int(self.combobox_coef_QAM.currentText())
+            self.QAM_input.setMaxLength(2**(pow-1))      
+            print(2**(pow-1))
+        else:
+            self.QAM_input.setMaxLength(32000)     
+             
+        if self.combobox_signal_type_QAM.currentText() == 'Binary':
+               self.combobox_decision_QAM.setEnabled(True)
+        if self.combobox_signal_type_QAM.currentText() == 'Integers':
+               self.combobox_decision_QAM.setEnabled(False)
+               self.QAM_input.setMaxLength(32000)
+               
+    def coef_handler(self):
+        if self.combobox_signal_type_QAM.currentText() == 'Binary':
+            if int(self.combobox_coef_QAM.currentText()) < 30:
+                pow = int(self.combobox_coef_QAM.currentText())
+                self.QAM_input.setMaxLength(2**(pow-1))      
+                print(2**(pow-1))
+        else:
+             self.QAM_input.setMaxLength(32000)      
+        
+    def check_apply_but_PSK(self):
+        # print(self.apply_PSK_but.isChecked())
+        PSK.runEngine()
+        if self.apply_PSK_but.isChecked() == True:
+            self.apply_QAM_but.setChecked(False)
+            PSK.runEngine()
+        
+    def draw_graph(self):
+        # plot_PSK.mainFunc()
+        
+        print(self.mes)
+        
+        self.plot_area_wid.canvas.axes.clear()
+        self.plot_area_wid.canvas.axes.plot([1]) 
+        
+        # self.plot_area_wid.canvas.axes.legend(('cosinus', 'sinus'),loc='upper right')
+        self.plot_area_wid.canvas.axes.set_title('График')
+        self.plot_area_wid.canvas.draw()
+        
+
+app = QApplication([])
+window = MatplotlibWidget()
+window.show()
+app.exec_()
