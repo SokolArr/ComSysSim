ccebb50366a6f8c122440dc679af58fd0786684c
SokolArr <axindri@gmail.com>
2022-11-26 19:49:14 +0300

    add rus coments

--- modulations/engine_PSK.py
+++ modulations/engine_PSK.py
@@ -1,9 +1,8 @@
-import random
-import numpy as np
-from ModulationPy import PSKModem
+import numpy as np                  #Импорт библиотек
+from ModulationPy import PSKModem   #Импорт библиотек
 
 def runEnginePSK (message, M_coef, mu, sigma, phase, grey_cod, bin_inp, soft_decis, bin_out):
-    
+    #Логгирование входных данных    
     print (' ')
     print ('Engine PSK Runs!')
     print('message:', message)
@@ -16,23 +15,28 @@ def runEnginePSK (message, M_coef, mu, sigma, phase, grey_cod, bin_inp, soft_dec
     print('soft_decis:', soft_decis)
     print('bin_out:', bin_out)
     
-    new_phase = np.pi / phase
-    
+    #Инициализация QAM модема
+    new_phase = np.pi / phase  #Пересчет фазы
     modem = PSKModem(M_coef, new_phase, grey_cod, bin_inp, soft_decis, bin_out)
+    
+    #Подготовка массива данных из сообщения 
     msg = np.array(message)
     
-    modulated = modem.modulate(msg) # modulation
+    #Запуск процесса модуляции
+    modulated = modem.modulate(msg) 
     new_modulated = np.copy(modulated)
     
+    #Инициализация шумовых переменных
     noise_real = np.random.normal(mu, sigma, len(msg))
     noise_imag = np.random.normal(mu, sigma, len(msg))
     
+    #Добавление шума
     for index in range(new_modulated.size):
         new_modulated[index] = new_modulated[index] + (noise_real[index] + noise_imag[index]*1j)
 
+    #Запуск процесса демодуляции
     demodulated = modem.demodulate(new_modulated) # demodulation
     
     print ('Engine PSK Stopped!')
-
     return modulated, new_modulated, demodulated, modem
     
--- modulations/engine_QAM.py
+++ modulations/engine_QAM.py
@@ -1,9 +1,8 @@
-import random
-import numpy as np
-from ModulationPy import QAMModem
+import numpy as np                  #Импорт библиотек
+from ModulationPy import QAMModem   #Импорт библиотек
 
 def runEngineQAM (message, M_coef, mu, sigma, grey_cod, bin_inp, soft_decis, bin_out):
-    
+    #Логгирование входных данных                        
     print (' ')
     print ('Engine QAM Runs!')
     print('message:', message)
@@ -14,22 +13,27 @@ def runEngineQAM (message, M_coef, mu, sigma, grey_cod, bin_inp, soft_decis, bin
     print('bin_inp:', bin_inp)
     print('soft_decis:', soft_decis)
     print('bin_out:', bin_out)
-   
+    
+    #Инициализация QAM модема
     modem = QAMModem(M_coef, grey_cod, bin_inp, soft_decis, bin_out)
     
+    #Подготовка массива данных из сообщения
     msg = np.array(message)
     
-    modulated = modem.modulate(msg) # modulation
+    #Запуск процесса модуляции
+    modulated = modem.modulate(msg)
     new_modulated = np.copy(modulated)
     
+    #Инициализация шумовых переменных
     noise_real = np.random.normal(mu, sigma, len(msg))
     noise_imag = np.random.normal(mu, sigma, len(msg))
     
+    #Добавление шума
     for index in range(new_modulated.size):
         new_modulated[index] = new_modulated[index] + (noise_real[index] + noise_imag[index]*1j)
-
-    demodulated = modem.demodulate(new_modulated) # demodulation
+        
+    #Запуск процесса демодуляции
+    demodulated = modem.demodulate(new_modulated)
     
     print ('Engine QAM Stopped!')
-    
     return modulated, new_modulated, demodulated, modem
